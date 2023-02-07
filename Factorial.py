c11e6aa3a5575fac86b5eb6c45da6ba6710cd8d6
Adarsh Kumar Singh <95336274+034adarsh@users.noreply.github.com>
2023-02-07 23:20:34 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -5,3 +5,18 @@ while(n>=fact):
   num*=fact
   fact+=1
 print(num) 
+
+
+#other method
+
+n=int(input('Enter a number:'))
+fact=1
+num=1
+if(n<0):
+  print('Not defined')
+ 
+else:
+  while(n>=fact):
+    num*=fact
+    fact+=1
+  print(num
